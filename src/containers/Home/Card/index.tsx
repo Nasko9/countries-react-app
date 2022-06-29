7602096154ce390223cdfa95f0d65ecb5c103c4b
Nasko9 <milosnaskovic17@gmail.com>
2022-06-29 09:36:20 +0200

    Created card component

---
+++ src/containers/Home/Card/index.tsx
@@ -0,0 +1,19 @@
+export default function Card() {
+  return (
+    <div className="h-80 w-[15.625rem] rounded-lg bg-secondary shadow-lg">
+      <div className="h-[9.375rem] rounded-t-lg bg-red-700"></div>
+      <div className="h-[10.625rem] rounded-b-lg px-[1.375rem] pt-[1rem]">
+        <h1 className="mb-[0.688rem] text-lg font-bold">Brazil</h1>
+        <p>
+          <span className="font-semibold">Population:</span> 206.135.893
+        </p>
+        <p>
+          <span className="font-semibold">Region:</span> Americas
+        </p>
+        <p>
+          <span className="font-semibold">Capital:</span> Brasilia
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,7 +1,21 @@
+// Components
+import Card from "./Card";
+
 export default function Home() {
   return (
     <div className="h-screen bg-primary text-primary">
-      <div className="container">Home</div>
+      <div className="container flex flex-wrap justify-center gap-[2.5rem] py-5 sm:justify-between sm:gap-[4.375rem]">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
+
+//! Za skeleton kartice: <div className="h-80 w-[15.6rem] rounded-lg bg-secondary"></div>;
